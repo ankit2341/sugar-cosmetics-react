eb2c186cfe922822dd88dbd9b929510544103320
ankit2341 <ankitpatil2341@gmail.com>
2022-10-12 19:08:53 +0530

    bestsellers

--- sugar-cosmetics/src/Pages/Home.jsx
+++ sugar-cosmetics/src/Pages/Home.jsx
@@ -1,11 +1,14 @@
+import BestSellers from "../components/HomePage/Bestsellers";
 import ControlledCarousel from "../components/HomePage/Carousal";
 import Navbar from "../components/Navbar";
+import "../styles/Home.css"
 
 export default function Home(){
     return (
      <>
      <Navbar/>
      <ControlledCarousel/>
+     <BestSellers/>
      <div style={{height:"1000px"}}></div>
      </>
     )
